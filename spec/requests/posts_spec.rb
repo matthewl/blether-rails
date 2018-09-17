95c572fad198928ab0a006e89108264a80b0173c
Matthew Lang <matthew@matthewlang.net>
2018-09-17 20:43:46 +0100

    Oops, put back in the other post specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -34,90 +34,90 @@ RSpec.describe 'Posts API', type: :request do
     end
   end
 
-  # describe 'GET /posts/:id' do
-  #   before { get "/posts/#{post_id}", params: {}, headers: headers }
+  describe 'GET /posts/:id' do
+    before { get "/posts/#{post_id}", params: {}, headers: headers }
 
-  #   context 'when the post exists' do
-  #     it 'returns the post' do
-  #       expect(json).not_to be_empty
-  #       expect(json['id']).to eq(post_id)
-  #     end
+    context 'when the post exists' do
+      it 'returns the post' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(post_id)
+      end
 
-  #     it 'returns a status code of 200' do
-  #       expect(response).to have_http_status(200)
-  #     end
-  #   end
+      it 'returns a status code of 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
 
-  #   context 'when post does not exist' do
-  #     let(:post_id) { 100 }
+    context 'when post does not exist' do
+      let(:post_id) { 100 }
 
-  #     it 'returns the status code of 404' do
-  #       expect(response).to have_http_status(404)
-  #     end
+      it 'returns the status code of 404' do
+        expect(response).to have_http_status(404)
+      end
 
-  #     it 'returns a not found message' do
-  #       expect(response.body).to match(/Couldn't find Post/) 
-  #     end
-  #   end
-  # end
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Post/) 
+      end
+    end
+  end
 
-  # describe 'POST /posts' do
-  #   # A valid post to create
-  #   let!(:account) { create(:account) }
-  #   let(:valid_attributes) { { title: 'Side-project update', body: "I'm going to make the next Twitter." }.to_json }
-  #   let(:invalid_attributes) { { body: '' }.to_json }
+  describe 'POST /posts' do
+    # A valid post to create
+    let!(:account) { create(:account) }
+    let(:valid_attributes) { { title: 'Side-project update', body: "I'm going to make the next Twitter." }.to_json }
+    let(:invalid_attributes) { { body: '' }.to_json }
 
-  #   context 'when the request is valid' do
-  #     before { post '/posts', params: valid_attributes, headers: headers }
+    context 'when the request is valid' do
+      before { post '/posts', params: valid_attributes, headers: headers }
 
-  #     it 'creates a post with a title' do
-  #       expect(json['title']).to eq('Side-project update')
-  #     end
+      it 'creates a post with a title' do
+        expect(json['title']).to eq('Side-project update')
+      end
 
-  #     it 'creates a post with a body' do
-  #       expect(json['body']).to eq("I'm going to make the next Twitter.")
-  #     end
+      it 'creates a post with a body' do
+        expect(json['body']).to eq("I'm going to make the next Twitter.")
+      end
 
-  #     it 'returns the status code of 201' do
-  #       expect(response).to have_http_status(201)
-  #     end
-  #   end
+      it 'returns the status code of 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
 
-  #   context 'when the request is invalid' do
-  #     before { post '/posts', params: invalid_attributes, headers: headers }
+    context 'when the request is invalid' do
+      before { post '/posts', params: invalid_attributes, headers: headers }
 
-  #     it 'returns the status code of 422' do
-  #       expect(response).to have_http_status(422)
-  #     end
+      it 'returns the status code of 422' do
+        expect(response).to have_http_status(422)
+      end
 
-  #     it 'returns a validation failure message' do
-  #       expect(response.body).to match(/Validation failed: Body can't be blank/)
-  #     end
-  #   end
-  # end
+      it 'returns a validation failure message' do
+        expect(response.body).to match(/Validation failed: Body can't be blank/)
+      end
+    end
+  end
 
-  # describe 'PUT /posts/:id' do
-  #   let!(:account) { create(:account) }
-  #   let(:valid_attributes) { { title: 'Side-project update', body: "I'm going to make the next Twitter and be a billionaire." }.to_json }
+  describe 'PUT /posts/:id' do
+    let!(:account) { create(:account) }
+    let(:valid_attributes) { { title: 'Side-project update', body: "I'm going to make the next Twitter and be a billionaire." }.to_json }
 
-  #   context 'when the record exists' do
-  #     before { put "/posts/#{post_id}", params: valid_attributes, headers: headers }
+    context 'when the record exists' do
+      before { put "/posts/#{post_id}", params: valid_attributes, headers: headers }
 
-  #     it 'updates the record' do
-  #       expect(response.body).to be_empty
-  #     end
+      it 'updates the record' do
+        expect(response.body).to be_empty
+      end
 
-  #     it 'returns the status code of 204' do
-  #       expect(response).to have_http_status(204)
-  #     end
-  #   end
-  # end
+      it 'returns the status code of 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
 
-  # describe 'DELETE /posts/:id' do
-  #   before { delete "/posts/#{post_id}", headers: headers }
+  describe 'DELETE /posts/:id' do
+    before { delete "/posts/#{post_id}", headers: headers }
 
-  #   it 'returns the status code of 204' do
-  #     expect(response).to have_http_status(204)
-  #   end
-  # end
+    it 'returns the status code of 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
 end
